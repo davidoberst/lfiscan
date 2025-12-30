d9dcf8ebad9d6681021e8c9ab561cf20d00193db
davidoberst <juandavidbustosperez82@gmail.com>
2025-12-30 14:58:44 -0500

    check http or https conection before fuzzing

--- lfiscan.py
+++ lfiscan.py
@@ -1,6 +1,9 @@
 import pyfiglet
 import argparse
-from icmplib import ping 
+import httpx
+import sys
+import time
+from colorama import init, Fore, Style; init(autoreset=True) 
 
 # PARSER / ARGUMENTS
 parser = argparse.ArgumentParser() #Create parser
@@ -8,18 +11,28 @@ parser.add_argument("-H",required=True,metavar="HOST",help="Host = http://exampl
 parser.add_argument("-W",required=True,metavar="WORDLIST",help="Wordlist") #wordlist
 args = parser.parse_args() #read user argument
 
-def CheckHost(): #check if the host is active before fuzz
- #OPTION 1 : Try with ICMP first
- host = args.H
- r = ping(host,count=2,timeout=2) #(r = result), count sends two packets, timeout waits 2 seconds 
- print("Host is up!" if r.is_alive else "The host appears to be down or unreachable. Please try again later.")
+def CheckHost(): # CHECK IF THE HOST EXIST OR IS ACTIVE BEFORE FUZZING
+ h = args.H.strip()
+ if not h.startswith(("https://","http://")):
+    time.sleep(1)
+    print("Unable to reach the host. Please provide a full URL (http:// or https://)")
+    sys.exit(1)
+ else:
+    try:
+       res = httpx.get(args.H, timeout=3.0, follow_redirects=True)
+       if res.status_code == 200:
+          print("[:] Host is up!")
+       else :
+          print("[:] Site does not exist or is not available (HTTP {res.status_code}).")
+    except httpx.RequestError:
+        print("[:] The host appears to be down or unreachable. Please try again later.")
  
-
-# BANNER
+# ---------------BANNER---------------------
 print(pyfiglet.figlet_format(text="lfiscan",font="larry3d"),end="")
 print("            https://github.com/davidoberst",end="")
 print("\n" + "-"*60 + "\n")
 
 print(f"[:] HOST : {args.H}")
 CheckHost()
+
 
